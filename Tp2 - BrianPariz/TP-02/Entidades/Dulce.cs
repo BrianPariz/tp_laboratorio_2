d236a89a6e6134823edc819edb33fed1d31d9718
Brian Pariz <brianpariz@hotmail.com>
2017-09-25 11:44:27 -0300

    Add files via upload

---
+++ Tp2 - BrianPariz/TP-02/Entidades/Dulce.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entidades_2017
+{
+    public class Dulce : Producto
+    {
+        /// <summary>
+        /// Los dulces tienen 80 calorías
+        /// </summary>
+        protected override short CantidadCalorias
+        {
+            get
+            {
+                return 80;
+            }
+        }
+
+
+        /// <summary>
+        /// Constructor
+        /// </summary>
+        /// <param name="marca"></param>
+        /// <param name="patente"></param>
+        /// <param name="color"></param>
+        public Dulce(EMarca marca, string patente, ConsoleColor color) : base(patente, marca, color) { }
+
+
+        /// <summary>
+        /// Muestro los datos del Dulce y de la clase que heredó
+        /// </summary>
+        /// <returns></returns>
+        public override string Mostrar()
+        {
+            StringBuilder sb = new StringBuilder();
+
+            sb.AppendLine("DULCE");
+            sb.AppendLine(base.Mostrar());
+            sb.AppendLine("CALORIAS : " + this.CantidadCalorias);
+            sb.AppendLine();
+            sb.AppendLine("---------------------");
+
+            return sb.ToString();
+        }
+    }
+}
---
+++ Tp2 - BrianPariz/TP-02/Entidades/Snacks.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entidades_2017
+{
+    public class Snacks : Producto
+    {
+
+        /// <summary>
+        /// Constructor
+        /// </summary>
+        /// <param name="marca"></param>
+        /// <param name="patente"></param>
+        /// <param name="color"></param>
+        public Snacks(EMarca marca, string patente, ConsoleColor color)
+            : base(patente, marca, color)
+        {
+        }
+
+        /// <summary>
+        /// Los snacks tienen 104 calorías
+        /// </summary>
+        protected override short CantidadCalorias
+        {
+            get
+            {
+                return 104;
+            }
+        }
+
+        /// <summary>
+        /// Muestro los datos del snack y de la clase que heredó
+        /// </summary>
+        /// <returns></returns>
+        public override string Mostrar()
+        {
+            StringBuilder sb = new StringBuilder();
+
+            sb.AppendLine("SNACKS");
+            sb.AppendLine(base.Mostrar());
+            sb.AppendLine("CALORIAS : " + this.CantidadCalorias);
+            sb.AppendLine();
+            sb.AppendLine("---------------------");
+
+            return sb.ToString();
+        }
+    }
+}
